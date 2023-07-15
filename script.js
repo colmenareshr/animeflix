80990d7b1bdfbbdaa23cfe477bef495fdc00e407
Humberto Colmenares <colmenareshr@gmail.com>
2023-07-15 18:27:06 -0300

    agregando colores dinámicos

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ fetch('https://kitsu.io/api/edge/trending/anime')
     console.log('Body:', data);
 
     const colors = [
-      { name: 'Vermelho', en: 'red', color: '#c83349' },
-      { name: 'Azul', en: 'blue', color: '#2472A4', textColor: '#f4f4f4' },
-      { name: 'Verde', en: 'green', color: '#138A72' },
-      { name: 'Branco', en: 'white' },
-      { name: 'Laranja', en: 'orange' },
-      { name: 'Roxo', en: 'purple', color: '#8E44AD' },
-      { name: 'Rosa', en: 'pink' },
-      { name: 'Cinza', en: 'grey' },
-      { name: 'Amarelo', en: 'yellow' },
-      { name: 'Marrom', en: 'brown' },
+      { name: 'Vermelho', en: 'red', color: '#c83349', textColor: '#ffffff' },
+      { name: 'Azul', en: 'blue', color: '#2472A4', textColor: '#ffffff' },
+      { name: 'Verde', en: 'green', color: '#138A72', textColor: '#ffffff' },
+      { name: 'Branco', en: 'white', color: '#FFFFFF', textColor: '#000000' },
+      { name: 'Laranja', en: 'orange', color: '#f2ae72', textColor: '#000000' },
+      { name: 'Roxo', en: 'purple', color: '#8E44AD', textColor: '#ffffff' },
+      { name: 'Rosa', en: 'pink', color: '#f9d5e5', textColor: '#000000' },
+      { name: 'Cinza', en: 'grey', color: '#d6d4e0', textColor: '#000000' },
+      { name: 'Amarelo', en: 'yellow', color: '#ffcc5c', textColor: '#000000' },
+      { name: 'Marrom', en: 'brown', color: '#a2836e', textColor: '#ffffff' },
     ];
 
     data.forEach((anime, index) => {
@@ -28,15 +28,15 @@ fetch('https://kitsu.io/api/edge/trending/anime')
       const { posterImage, titles, synopsis } = anime.attributes;
       //Mostramos un número de palabras para la sinopsis
       const maxWords = 60;
-      const shortSynopsis = synopsis.split(' ').slice(0, maxWords).join(' ')
+      const shortSynopsis = synopsis.split(' ').slice(0, maxWords).join(' ');
       //Fin//
 
-      //mostrar el título de colores en capitalize 
+      //mostrar el título de colores en capitalize
       const colorNameEn = colors[index % colors.length].en;
-      const titleNameColorEn = colorNameEn.charAt(0).toUpperCase() + colorNameEn.slice(1) 
+      const titleNameColorEn =
+        colorNameEn.charAt(0).toUpperCase() + colorNameEn.slice(1);
 
-
-      const backgroundColor = colors[index % colors.length];
+      const backgroundTextColor = colors[index % colors.length];
 
       animeCard.innerHTML = `
         <div class="flip-card-inner">
@@ -44,10 +44,11 @@ fetch('https://kitsu.io/api/edge/trending/anime')
             <img src="${posterImage.large}" alt="Poster de ${titles.en}" class="border" style="padding: 12px; width: 80%">
             <h2>${titleNameColorEn}</h2>
           </div>
-          <div class="flip-card-back" style="background-color: ${backgroundColor.color}" "color:${backgroundColor.textColor}" >
-            <h3> ${titles.en} </h3>
+          <div class="flip-card-back" style="background-color: ${backgroundTextColor.color}; color:${backgroundTextColor.textColor}" >
+            <h3 style= "color: ${backgroundTextColor.textColor}; font-size: 1.3rem" > ${titles.en} </h3>
             <p> ${shortSynopsis} </p>
-            <h4> ${backgroundColor.name} </h4>
+            <a href="anime.html ">Mais informação</a>
+            <h4> ${backgroundTextColor.name} </h4>
           </div>
         </div>
       `;
