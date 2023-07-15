2f815ed43699b51dae35a6ad9399dfe1a25d0925
Humberto Colmenares <colmenareshr@gmail.com>
2023-07-14 21:51:39 -0300

    agregando funcionalidad

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 fetch('https://kitsu.io/api/edge/trending/anime')
   .then((response) => {
     console.log('Status:', response.status);
@@ -12,22 +14,28 @@ fetch('https://kitsu.io/api/edge/trending/anime')
       const animeCard = document.createElement('div');
       animeCard.classList.add('flip-card');
 
-      const { posterImage, titles } = anime.attributes;
+      const { posterImage, titles, synopsis } = anime.attributes;
+
+      const colors = ['white', 'blue'];
+
+      colors.map((color) => {
+        const colorsCard = color;
+      });
 
       animeCard.innerHTML = `
-      <div class="flip-card">
+      
           <div class="flip-card-inner">
             <div class="flip-card-front">
               <img src="${posterImage.large}" alt="${titles.en}" class="w3-border" style="padding: 12px; width: 80%">
-              <h2>${titles.en}</h2>
+              <h2>blue</h2>
             </div>
             <div class="flip-card-back">
               <h2>${titles.en}</h2>
               <p>Synopsis</p>
-              <p>XXXXXXXXX</p>
+              <p>${synopsis}</p>
             </div>
           </div>
-        </div>
+        
       `;
       cardContainer.appendChild(animeCard);
     });
